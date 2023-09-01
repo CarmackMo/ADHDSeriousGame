e851a48f331b3ef60040bea9c85d88df0764234a
WaterFriend <116010169@link.cuhk.edu.cn>
2023-09-01 17:37:27 -0600

    Update Singleton.cs

--- SourceCode/Assets/Scripts/Tools/Singleton.cs
+++ SourceCode/Assets/Scripts/Tools/Singleton.cs
@@ -11,30 +11,15 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     protected virtual void Awake()
     {
         if (instance == null)
-        {
             instance = GameObject.FindObjectOfType<T>();
-        }
 
-        if (instance != null && instance.gameObject.GetInstanceID() == GetInstanceID())
+        if (instance != null && instance.GetInstanceID() != this.GetInstanceID())
             Destroy(gameObject);
     }
 
+    protected virtual void Start() { }
 
-    protected virtual void Start()
-    {
+    protected virtual void Update() { }
 
-    }
-
-
-    protected virtual void Update()
-    {
-
-    }
-
-
-    protected virtual void OnDestroy()
-    {
-        instance = null;
-
-    }
+    protected virtual void OnDestroy() { }
 }
